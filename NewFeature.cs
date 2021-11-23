d7813d1bd20da4d2fe3199ae9522847e7b174f72
Robert Steiman <robertsteiman@op.pl>
2021-11-23 13:54:08 +0100

    początki

---
+++ NewFeature.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyCSharp
+{
+    public class NewFeature
+    {
+        private static void Main(string[] args)
+        {
+
+        }
+    }
+}
---
+++ Parsowanie.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UdemyCSharp
+{
+    public class Parsowanie
+    {
+        //private static void Main(string[] args)
+        //{
+        //    //var someNumberInString = "5";
+        //    //var someNumberInInt = int.Parse(someNumberInString);
+        //    //var someNumber = int.Parse(Console.ReadLine());czyta linie z konsoli
+        //    var isSuccess = int.TryParse(Console.ReadLine(), out var result); // teraz jeśli użytkownik wpisze niedający się sparsować tekst to dostajemy  wartość true albo false zamiast exception jak w przypadku wyżej
+        //    if(isSuccess)
+        //    {
+        //        Console.WriteLine(result);
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine("Inccorect format.");
+        //    }
+        //}
+    }
+}
